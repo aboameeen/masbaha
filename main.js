7523670a6819b013a985edd58d4412a8a6f1b5e4
aboameeen <108266365+aboameeen@users.noreply.github.com>
2022-09-02 17:04:20 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,141 @@
+function add1() {
+  let txt1 = document.getElementById("t1").value;
+  document.getElementById("t1").value = +txt1 + 1;
+}
+function reset1() {
+  document.getElementById("t1").value = 0;
+}
+//>>>>>>>>>>>>>>>>
+function add2() {
+  let txt1 = document.getElementById("t2").value;
+  document.getElementById("t2").value = +txt1 + 1;
+}
+function reset2() {
+  document.getElementById("t2").value = 0;
+}
+
+//>>>>>>>>>>>>>>>>
+function add3() {
+  let txt1 = document.getElementById("t3").value;
+  document.getElementById("t3").value = +txt1 + 1;
+}
+function reset3() {
+  document.getElementById("t3").value = 0;
+}
+
+//>>>>>>>>>>>>>>>>
+
+function add4() {
+  let txt1 = document.getElementById("t4").value;
+  document.getElementById("t4").value = +txt1 + 1;
+}
+function reset4() {
+  document.getElementById("t4").value = 0;
+}
+//>>>>>>>>>>>>>>>>
+function add5() {
+  let txt1 = document.getElementById("t5").value;
+  document.getElementById("t5").value = +txt1 + 1;
+}
+function reset5() {
+  document.getElementById("t5").value = 0;
+}
+//>>>>>>>>>>>>>>>>
+function add6() {
+  let txt1 = document.getElementById("t6").value;
+  document.getElementById("t6").value = +txt1 + 1;
+}
+function reset6() {
+  document.getElementById("t6").value = 0;
+}
+//>>>>>>>>>>>>>>>>
+function add7() {
+  let txt1 = document.getElementById("t7").value;
+  document.getElementById("t7").value = +txt1 + 1;
+}
+function reset7() {
+  document.getElementById("t7").value = 0;
+}
+//>>>>>>>>>>>>>>>>
+function add8() {
+  let txt1 = document.getElementById("t8").value;
+  document.getElementById("t8").value = +txt1 + 1;
+}
+function reset8() {
+  document.getElementById("t8").value = 0;
+}
+//>>>>>>>>>>>>>>>>
+function add9() {
+  let txt1 = document.getElementById("t9").value;
+  document.getElementById("t9").value = +txt1 + 1;
+}
+function reset9() {
+  document.getElementById("t9").value = 0;
+}
+//>>>>>>>>>>>>>>>>
+function add10() {
+  let txt1 = document.getElementById("t10").value;
+  document.getElementById("t10").value = +txt1 + 1;
+}
+function reset10() {
+  document.getElementById("t10").value = 0;
+}
+//>>>>>>>>>>>>>>>>
+
+function add11() {
+  let txt1 = document.getElementById("t11").value;
+  document.getElementById("t11").value = +txt1 + 1;
+}
+function reset11() {
+  document.getElementById("t11").value = 0;
+}
+//>>>>>>>>>>>>>>>>
+function add12() {
+  let txt1 = document.getElementById("t12").value;
+  document.getElementById("t12").value = +txt1 + 1;
+}
+function reset12() {
+  document.getElementById("t12").value = 0;
+}
+//>>>>>>>>>>>>>>>>
+function add13() {
+  let txt1 = document.getElementById("t13").value;
+  document.getElementById("t13").value = +txt1 + 1;
+}
+function reset13() {
+  document.getElementById("t13").value = 0;
+}
+//>>>>>>>>>>>>>>>>
+function add14() {
+  let txt1 = document.getElementById("t14").value;
+  document.getElementById("t14").value = +txt1 + 1;
+}
+function reset14() {
+  document.getElementById("t14").value = 0;
+}
+//>>>>>>>>>>>>>>>>
+function add15() {
+  let txt1 = document.getElementById("t15").value;
+  document.getElementById("t15").value = +txt1 + 1;
+}
+function reset15() {
+  document.getElementById("t15").value = 0;
+}
+//>>>>>>>>>>>>>>>>
+function tasfeer() {
+  document.getElementById("t1").value = 0;
+  document.getElementById("t2").value = 0;
+  document.getElementById("t3").value = 0;
+  document.getElementById("t4").value = 0;
+  document.getElementById("t5").value = 0;
+  document.getElementById("t6").value = 0;
+  document.getElementById("t7").value = 0;
+  document.getElementById("t8").value = 0;
+  document.getElementById("t9").value = 0;
+  document.getElementById("t10").value = 0;
+  document.getElementById("t11").value = 0;
+  document.getElementById("t12").value = 0;
+  document.getElementById("t13").value = 0;
+  document.getElementById("t14").value = 0;
+  document.getElementById("t15").value = 0;
+}
